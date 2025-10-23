099128ed61336263542cc9d32fd992a020f2934c
Pamela Raiane <batista.paula.pamela@escola.pr.gov.br>
2025-10-23 14:10:47 +0000

    kik

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ScrollView } from "react-native";
+import Titulo from "./componentes/Titulo";
+import Saudacao from "./componentes/Saudacao";
+
+export default function App(){
+return(
+  <ScrollView style={{padding:20}}>
+    <Titulo/>
+    
+    <Saudacao/>
+  </ScrollView>
+)
+}
